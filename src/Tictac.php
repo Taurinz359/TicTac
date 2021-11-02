f028fae16b22eb1bd5cb71c61625b0b2d0e2454e
Taurinz359 <workshere359@gmail.com>
2021-11-02 12:07:00 +0300

    review update

--- src/Tictac.php
+++ src/Tictac.php
@@ -21,42 +21,45 @@ function startGame()
         " "," "," ",
         " "," "," "
     ];
-    $i = 0;
-    while ($i < 9) {
-        if ($i % 2 === 0) {
+    $moveNumber = 0;
+    $maxSteps = 9;
+    while ($moveNumber < $maxSteps) {
+        $evenOdd = $moveNumber % 2;
+        if ($evenOdd === 0) {
             line("\nMove X: ");
         } else {
             line("\nMove O: ");
         }
-        $position = readnCheckSymbol();
-        --$position;
+        $position = getPositionFromInput();
         if ($currentGameState[$position ] !== " ") {
             line("This position is busy, Try again!");
             continue;
         }
+
         system('clear');
 
-        if ($i % 2 === 0) {
+        if ($evenOdd === 0) {
             $currentGameState[$position] = "X";
         } else {
             $currentGameState[$position] = "O";
         }
         showCurrentGameStateTable($currentGameState);
-        if (3 < $i) {
+
+        if (3 < $moveNumber) {
             $winner = checkWinner($currentGameState);
             if ($winner != "null"){
                 line($winner);
                 exit;
             }
         }
-        ++$i;
+        ++$moveNumber;
     }
     line("oOps it is Draw! :c ");
 }
 
 function checkWinner(array $currentGameState):string
 {   
-    $winPosition = [
+    $winPositions = [
         [0,1,2],
         [0,3,6],
         [0,4,8],
@@ -69,12 +72,12 @@ function checkWinner(array $currentGameState):string
     $xOccurs = 0;
     $oOccurs = 0;
 
-    for ($i=0; $i < count($winPosition); $i++) { 
-        for ($j=0; $j < count($winPosition[$i]); $j++) { 
-            if($currentGameState[$winPosition[$i][$j]] === "X"){ 
+    for ($i=0; $i < count($winPositions); $i++) { 
+        for ($j=0; $j < count($winPositions[$i]); $j++) { 
+            if($currentGameState[$winPositions[$i][$j]] === "X"){ 
                 ++$xOccurs;
             }
-            elseif($currentGameState[$winPosition[$i][$j]] === "O"){
+            elseif($currentGameState[$winPositions[$i][$j]] === "O"){
                 ++$oOccurs;
             }
         }
@@ -113,8 +116,7 @@ function showTutorial()
 }
 
 function showCurrentGameStateTable(array $XOdataArray)
-{
-//this function drawing currentGameState
+{//this function drawing currentGameState
     $XOArray = [
         "=============",
         "       +---+---+---+",
@@ -131,11 +133,11 @@ function showCurrentGameStateTable(array $XOdataArray)
     }
 }
 
-function readnCheckSymbol() : int
+function getPositionFromInput() : int
 {
     $userNum = prompt("choice num 1-9 ");
     while ($userNum <= 0 || $userNum > 9) {        
         $userNum = prompt ("Please read only 1-9 ");
     }
-    return $userNum;
+    return --$userNum;
 }
